d3e54c901b83392a1b83eca2285ab3dd1a415e8c
Samuel Lambert <igweajurijosph@gmail.com>
2026-04-07 22:03:39 +0100

    feat: orders page, smartsupp chat, navbar fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </WishlistProvider>
         </CartProvider>
         </AuthProvider>
+        <script dangerouslySetInnerHTML={{ __html: `var _smartsupp = _smartsupp || {};_smartsupp.key = 'ea145b844873a3205b73a815040e510269980a9f';window.smartsupp||(function(d){var s,c,o=smartsupp=function(){o._.push(arguments)};o._=[];s=d.getElementsByTagName('script')[0];c=d.createElement('script');c.type='text/javascript';c.charset='utf-8';c.async=true;c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);})(document);` }} />
       </body>
     </html>
   );
